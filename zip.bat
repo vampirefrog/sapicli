0d400b1c2a03d82bc2b775d935904e47e87ba48a
vampi <vampi@vampi.tech>
2022-12-14 22:08:42 +0200

    zip bat

--- zip.bat
+++ zip.bat
@@ -4,4 +4,4 @@ SET V=0.4
 pushd Win32\ReleaseStatic && (%SZ% a ..\..\sapicli-%V%-x86-static.zip sapicli.exe & popd)
 pushd Win32\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x86.zip sapicli.exe ogg.dll libvorbis.dll opus.dll & popd)
 pushd x64\ReleaseStatic && (%SZ% a ..\..\sapicli-%V%-x64-static.zip sapicli.exe & popd)
-pushd x64\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x64.zip sapicli.exe libvorbis.dll opus.dll & popd)
+pushd x64\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x64.zip sapicli.exe ogg.dll libvorbis.dll opus.dll & popd)
