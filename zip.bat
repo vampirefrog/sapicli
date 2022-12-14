0e2116d006d9a1781335dfe740cf1f69459727fb
vampi <vampi@vampi.tech>
2022-12-14 22:11:04 +0200

    0.5

--- zip.bat
+++ zip.bat
@@ -1,5 +1,5 @@
 SET SZ="C:\Program Files\7-Zip\7z.exe"
-SET V=0.4
+SET V=0.5
 
 pushd Win32\ReleaseStatic && (%SZ% a ..\..\sapicli-%V%-x86-static.zip sapicli.exe & popd)
 pushd Win32\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x86.zip sapicli.exe ogg.dll libvorbis.dll opus.dll & popd)
