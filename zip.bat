c0d8824b3d6c0c0ab814cad60b06f36f52d0005a
vampi <vampi@vampi.tech>
2022-12-27 09:05:27 +0200

    prepare release 0.6

--- zip.bat
+++ zip.bat
@@ -1,7 +1,7 @@
 SET SZ="C:\Program Files\7-Zip\7z.exe"
-SET V=0.5
+SET V=0.6
 
 pushd Win32\ReleaseStatic && (%SZ% a ..\..\sapicli-%V%-x86-static.zip sapicli.exe & popd)
-pushd Win32\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x86.zip sapicli.exe ogg.dll libvorbis.dll opus.dll & popd)
+pushd Win32\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x86.zip sapicli.exe ogg.dll libvorbis.dll opus.dll libmp3lame.dll & popd)
 pushd x64\ReleaseStatic && (%SZ% a ..\..\sapicli-%V%-x64-static.zip sapicli.exe & popd)
-pushd x64\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x64.zip sapicli.exe ogg.dll libvorbis.dll opus.dll & popd)
+pushd x64\ReleaseDynamic && (%SZ% a ..\..\sapicli-%V%-x64.zip sapicli.exe ogg.dll libvorbis.dll opus.dll libmp3lame.dll & popd)
