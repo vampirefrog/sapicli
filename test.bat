3b5e97487b03661ab69c4e1e99b1799c5a6d773a
vampi <vampi@vampi.tech>
2022-12-27 08:35:09 +0200

    test mp3

--- test.bat
+++ test.bat
@@ -3,7 +3,7 @@ setlocal enableDelayedExpansion
 SET TEXT="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 FOR %%P IN (Win32 x64) DO (
 	FOR %%C IN (ReleaseStatic) DO (
-		FOR %%F IN (raw wav ogg+vorbis ogg+opus) DO (
+		FOR %%F IN (raw wav mp3 ogg+vorbis ogg+opus) DO (
 			SET EXT=%%F
 			IF !EXT!==ogg+vorbis SET EXT=ogg
 			IF !EXT!==ogg+opus SET EXT=ogg
